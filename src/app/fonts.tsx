3f77dbe640878754f6875a6f5c61def994dda5f1
nanati45 <nansiasamnew@gmail.com>
2025-03-01 01:04:13 -0800

    change hub setup

---
+++ src/app/fonts.tsx
@@ -0,0 +1,31 @@
+import { Manrope } from "next/font/google";
+import localFont from "next/font/local";
+
+export const manrope = Manrope({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const clashBold = localFont({
+  src: "./fonts/ClashGrotesk-Bold.woff2",
+});
+
+export const gilroyExtraBold = localFont({
+  src: "./fonts/Gilroy-ExtraBold.woff2",
+});
+
+export const gilroyBold = localFont({
+  src: "./fonts/Gilroy-Bold.woff2",
+});
+export const gilroySemiBold = localFont({
+  src: "./fonts/Gilroy-Semibold.woff2",
+});
+export const gilroyMedium = localFont({
+  src: "./fonts/Gilroy-Medium.woff2",
+});
+export const gilroyRegular = localFont({
+  src: "./fonts/Gilroy-Regular.woff2",
+});
+export const gilroyThin = localFont({
+  src: "./fonts/Gilroy-Thin.woff2",
+});
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { clashBold } from "@/app/fonts";
+const page = () => {
+  return (
+    <div className={`${clashBold.className} text-[36px] text-headings`}>
+      {" "}
+      hello
+    </div>
+  );
+};
+
+export default page;
