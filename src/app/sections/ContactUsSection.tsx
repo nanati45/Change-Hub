37d0cdab61edb75280f69d8243a6754b1febc22a
nanati45 <nansiasamnew@gmail.com>
2025-03-05 17:13:47 +0300

    animating the pages

--- src/app/sections/ContactUsSection.tsx
+++ src/app/sections/ContactUsSection.tsx
@@ -1,8 +1,5 @@
-import { SendIcon } from "@/components/Icons";
-import { Button } from "@heroui/button";
 import Image from "next/image";
 import React from "react";
-import { gilroyBold, gilroyRegular } from "../fonts";
 import SendEmail from "@/components/SendEmail";
 import ContactDetails from "@/components/ContactDetails";
 
